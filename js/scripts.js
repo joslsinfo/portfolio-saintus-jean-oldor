e8ae9f25f9460cc5b33a932733cdcac9aca370fa
Jean Oldor Saintus <josaintus@gmail.com>
2023-10-09 14:40:58 -0400

    Update portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 let darkMode = false;
 
 function changeDarkMode() {
+  darkMode = !darkMode; // Inversion de la valeur de darkMode
+
+  // Utilisation d'une seule instruction if pour gérer les deux cas
   if (darkMode) {
-    darkMode = false;
-    document.documentElement.style.setProperty("--text-color", "black");
-    document.documentElement.style.setProperty("--background-color", "#f7f7f7");
-    document.getElementById("dark-light-mode").innerHTML = "Dark mode";
-  } else {
-    darkMode = true;
     document.documentElement.style.setProperty("--text-color", "white");
     document.documentElement.style.setProperty("--background-color", "black");
-    document.getElementById("dark-light-mode").innerHTML = "Light mode";
+  } else {
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--background-color", "#f7f7f7");
   }
+
+  // Mise à jour du texte en fonction de darkMode
+  const modeLabel = document.getElementById("dark-light-mode");
+  modeLabel.innerHTML = darkMode ? "Light mode" : "Dark mode";
 }
